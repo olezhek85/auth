2f3aa279bebd1364a46f7bc45bbc12da0557f25e
olezhek85 <olezhek_85@list.ru>
2016-11-06 17:55:41 +0200

    Project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ProtectedComponent } from './protected/protected.component';
+import { HeaderComponent } from './shared/header.component';
+import { SigninComponent } from './unprotected/signin.component';
+import { SignupComponent } from './unprotected/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProtectedComponent,
+    HeaderComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
