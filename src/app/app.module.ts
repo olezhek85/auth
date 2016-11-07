7116bad2eae5c811925324e559fe90387e33f3c2
olezhek85 <olezhek_85@list.ru>
2016-11-07 21:35:50 +0200

    Protecting a Route from Unauthorized Access

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { ProtectedComponent } from './protected/protected.component';
-import { HeaderComponent } from './shared/header.component';
-import { SigninComponent } from './unprotected/signin.component';
-import { SignupComponent } from './unprotected/signup.component';
+import {AppComponent} from './app.component';
+import {ProtectedComponent} from './protected/protected.component';
+import {HeaderComponent} from './shared/header.component';
+import {SigninComponent} from './unprotected/signin.component';
+import {SignupComponent} from './unprotected/signup.component';
 import {routing} from "./app.routes";
 import {AuthService} from "./shared/auth.service";
+import {AuthGuard} from "./shared/auth.guard";
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import {AuthService} from "./shared/auth.service";
     HttpModule,
     routing
   ],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
